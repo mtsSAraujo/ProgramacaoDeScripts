b6d38cf24bd7f92c95367ef757714bdc6bd6e38c
MATEUS SILVA ARAUJO <1090482311007@fatec-srv.local>
2025-03-10 20:26:09 -0300

    outro metodo para colocar as linhas

--- Aula04/desafio-dama/app/components/Linha.js
+++ Aula04/desafio-dama/app/components/Linha.js
@@ -3,7 +3,7 @@ import style from "@/app/styles/Linha.module.css"
 
 export default function Linha(props) {
     return (
-        <div className={style.linha}>
+        <div className={style.linha}>{/*
             <Subdivisao preta = {props.preta} peca = {props.peca && props.preta} jogador2 = {props.jogador2}/>
             <Subdivisao preta = {!props.preta} peca = {props.peca && !props.preta} jogador2 = {props.jogador2}/>
             <Subdivisao preta = {props.preta} peca = {props.peca && props.preta} jogador2 = {props.jogador2}/>
@@ -12,6 +12,15 @@ export default function Linha(props) {
             <Subdivisao preta = {!props.preta} peca = {props.peca && !props.preta} jogador2 = {props.jogador2}/>
             <Subdivisao preta = {props.preta} peca = {props.peca && props.preta} jogador2 = {props.jogador2}/>
             <Subdivisao preta = {!props.preta} peca = {props.peca && !props.preta} jogador2 = {props.jogador2}/>
+            */}
+            {
+                Array.from({length: 8}).map((_, i) => (
+                    <Subdivisao
+                        key = {i}
+                        preta= {i % 2 === 0 ? props.preta : !props.preta}
+                    />
+                ))
+            }
         </div>
     );
 }
