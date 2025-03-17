0aeed4782d3eec35c7cfd6227209c396cd1dfe6e
MATEUS SILVA ARAUJO <1090482311007@fatec-srv.local>
2025-03-17 19:41:21 -0300

    Novo projeto aula 5, com orientação a objetos

---
+++ Aula05/segundo-app/model/Produto.js
@@ -0,0 +1,23 @@
+class Produto {
+    #id;
+    #nome;
+    #preco;
+
+    constructor(id, nome, preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get nome() {
+        return this.#nome;
+    }
+
+    get preco() {
+        return this.#preco;
+    }
+}
